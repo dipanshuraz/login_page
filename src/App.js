437a1f19fcb423cab13eac02ae4629c5e611e6ae
dipanshuraz <uxg.deepanshu@gmail.com>
2021-07-27 19:55:35 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      HELLO
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
