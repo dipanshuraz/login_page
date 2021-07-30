b10a491f70d12d578a9a9bb3794dc34ce38d166f
dipanshuraz <uxg.deepanshu@gmail.com>
2021-07-30 18:21:36 +0530

    sass

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components/macro";
+import "./styles.scss";
 
 const ButtonWrapper = styled.button`
-  display: block;
-  margin-top: 0.75rem;
-  width: 100%;
   background-color: ${(props) => props.bg};
   color: ${(props) => props.color};
-  border: none;
-  outline: none;
-  font-family: inherit;
-  font-size: 1.25rem;
-  font-weight: 600;
-  padding: 0.75rem 0;
-  border-radius: 0.5rem;
-  cursor: pointer;
-  opacity: 0.95;
-
   &:hover {
     opacity: 1;
   }
@@ -30,7 +18,7 @@ const Button = ({ bg, color, children, classes, onClick }) => (
     bg={bg}
     type="button"
     onClick={onClick}
-    className={classes}>
+    className={`custom-button ${classes}`}>
     {children}
   </ButtonWrapper>
 );
