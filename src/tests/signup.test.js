503500a4ea988e2e80f61ae2c4894df772dcaa85
gauthamhc <gautham.hc@gmail.com>
2021-08-03 15:55:23 +0530

    register page testing

---
+++ src/tests/signup.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Signup from "../components/pages/Signup";
+
+describe("SignUp Page", () => {
+  test("renders Signup heading", () => {
+    render(
+      <BrowserRouter>
+        <Signup />
+      </BrowserRouter>,
+    );
+    const headerComponent = screen.getByText("Register");
+    expect(headerComponent).toBeInTheDocument();
+  });
+
+  test("rendering for sign up button", () => {
+    render(
+      <BrowserRouter>
+        <Signup />
+      </BrowserRouter>,
+    );
+    const signupButtonComponent = screen.getByRole("button", {
+      name: "Sign In",
+    });
+    expect(signupButtonComponent).toBeInTheDocument();
+  });
+});
